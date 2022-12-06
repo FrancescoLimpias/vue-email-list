0f30108341034e8c0b8b3e5e41f855bb2cedf045
Francesco Limpias <francescolimpias@gmail.com>
2022-12-06 17:27:09 +0100

    Exercise

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ const { createApp } = Vue
 const App = createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            emailsN: 10,
+            emails: [],
         }
-    }
+    },
+
+    methods: {
+
+        fetchEmail() {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+                (res) => {
+                    this.emails.push(res.data.response);
+                }
+            );
+        }
+    },
+
+    mounted() {
+        for (let i = 0; i < this.emailsN; i++) {
+            this.fetchEmail();
+        }
+    },
 });
+
